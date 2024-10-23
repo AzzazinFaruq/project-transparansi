3251252ee3a50d5866e186584ef096a9862ae597
AzzazinFaruq <faruq7j@gmail.com>
2024-10-23 15:16:35 +0700

    setting setup database

--- golang-restful-api/setup/setup.go
+++ golang-restful-api/setup/setup.go
@@ -11,6 +11,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() { //username:password(if exist)@tcp(database URL)/database_name
+	
 	database, err := gorm.Open(mysql.Open("root:@tcp(127.0.0.1:3306)/db_project_tranparansi_publik?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
