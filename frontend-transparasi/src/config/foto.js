239b66b4bca4454ce137584d3dc78fe789c3bc8d
AzzazinFaruq <faruq7j@gmail.com>
2024-11-17 20:12:03 +0700

    update revisi

--- frontend-transparasi/src/config/foto.js
+++ frontend-transparasi/src/config/foto.js
@@ -1,4 +1,5 @@
-export const BASE_URL = os.env.BE_URL || 'http://localhost:8000';
+
+export const BASE_URL =  'http://localhost:8000';
 export const getImageUrl = (path) => {
   if (!path) return '';
   if (path.startsWith('http')) return path;
